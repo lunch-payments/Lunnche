782a6385e7fac574ad6d4954dee2dae6340fe1e9
hiszpan <veakabori@gmail.com>
2016-05-10 09:26:04 +0200

    Zrobiony początkowy projekt togglerów do dodawania dań w zamówieniach
    Kod JS jest bardzo prototypowy, i napisany po to by pokazać samo
    działanie pokazywania się list

--- hiszpen/assets/js/main.js
+++ hiszpen/assets/js/main.js
@@ -1,11 +1,46 @@
 $(".order-addItem-toggler").click(function() {
-  if ($(".order-addItem").hasClass("active")) {
-    $(".order-addItem").removeClass("active");
-    $(".order-addItem").css("top", 65);
-  } else {
-    var scrollTop = $(window).scrollTop();
+    if ($(".order-addItem").hasClass("active")) {
+        $(".order-addItem").removeClass("active");
+        $(".order-addItem").css("top", 65);
+    } else {
+        var scrollTop = $(window).scrollTop();
 
-    $(".order-addItem").addClass("active");
-    $(".order-addItem").css("top", scrollTop + 65);
-  }
+        $(".order-addItem").addClass("active");
+        $(".order-addItem").css("top", scrollTop + 65);
+    }
 });
+
+//TODO: dokończenie togglera żeby działał
+
+function toggleOrderNewItemMenus(menuType) {
+    var menuTypeUp = function() {
+        return menuType.charAt(0).toUpperCase() + menuType.slice(1);
+    };
+    $(document).on("click", ".order-newItem-" + menuType, function() {
+        var state = $(this).data('state-' + menuType);
+        var switchOn = function() {
+            $(".order-newItem-" + menuType + "-text").hide();
+            $(".order-newItem-" + menuType + " > input").show();
+            $(".order-choose" + menuTypeUp()).show();
+        };
+        var switchOff = function() {
+            $(".order-newItem-" + menuType + "-text").show();
+            $(".order-newItem-" + menuType + " > input").hide();
+            $(".order-choose" + menuTypeUp()).hide();
+        };
+
+        switch (state) {
+            case 1:
+            case undefined:
+                switchOn();
+                $(this).data('state-' + menuType, 2);
+                break;
+            case 2:
+                switchOff();
+                $(this).data('state-' + menuType, 1);
+                break;
+        }
+    });
+}
+toggleOrderNewItemMenus("comp");
+toggleOrderNewItemMenus("dish");
